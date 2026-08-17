7e8777a4456a54f01aba059f52bb957417a1352d
PrasannaHegde11 <hegdeprasanna11@gmail.com>
2026-08-17 11:48:37 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@/components/Header';
 import { HeroSection } from '@/components/HeroSection';
+import { AwardsStrip } from '@/components/AwardsStrip';
 import { AboutSection } from '@/components/AboutSection';
 import { FeaturedProjectsSection } from '@/components/FeaturedProjectsSection';
 import { MoreProjectsSection } from '@/components/MoreProjectsSection';
 import { MethodologySection } from '@/components/MethodologySection';
 import { CareerSection } from '@/components/CareerSection';
 import { EducationSection } from '@/components/EducationSection';
+import { TestimonialsSection } from '@/components/TestimonialsSection';
 import { ContactSection } from '@/components/ContactSection';
 import { Footer } from '@/components/Footer';
 import { BackToTop } from '@/components/BackToTop';
@@ -18,12 +20,14 @@ const Index = () => {
       <Header />
       <main id="main-content">
         <HeroSection />
+        <AwardsStrip />
         <AboutSection />
         <FeaturedProjectsSection />
         <MoreProjectsSection />
         <MethodologySection />
         <CareerSection />
         <EducationSection />
+        <TestimonialsSection />
         <ContactSection />
       </main>
       <Footer />
